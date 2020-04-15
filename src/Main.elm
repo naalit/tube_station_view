ca640421f74b96f12c173b939061bc3f7be24e09
Lorxu <sam@dirkback.com>
2020-04-15 16:07:20 -0500

    Display on a sign in a picture The picture is the "underground station of the future", not a current one, but it was the only one with a nice view of the arrivals display

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Time
 import Task
 import Element as E
 import Element.Background as Background
+import Element.Font as Font
 import Http
 import Json.Decode as D
 
@@ -77,10 +78,30 @@ show_fail e = case e of
   Http.BadBody x -> "bad body:\n" ++ x
 
 --view : Model -> Html.Html msg
-view m = E.layout [E.width E.fill, E.height E.fill, E.centerX, E.centerY] <| case m of
+view m = E.layout [E.behindContent (E.image [E.centerX, E.centerY] { src = "/station.png", description = "" }), E.width E.fill, E.height E.fill, E.centerX, E.centerY] <| case m of
   Loading -> E.text "Loading"
   Failure e -> E.text ("Failed because of " ++ e)
-  Data d -> E.column [E.height (E.px 10), E.centerX, E.centerY, E.spacing 5] [something d.entries, view_time d.time]
+  Data d -> E.column
+    [ E.height (E.px 10)
+    , E.centerX
+    , E.centerY
+    , E.spacing 1
+    , E.paddingEach
+      { top = 0
+      , right = 70
+      , bottom = 550
+      , left = 0
+      }
+    , Font.color (E.rgb255 231 226 72)
+    , Font.size 15
+    ] [something d.entries, view_time d.time]
+
+edges =
+  { top = 0
+  , right = 0
+  , bottom = 0
+  , left = 0
+  }
 
 view_time : Time.Posix -> E.Element msg
 view_time time =
@@ -90,7 +111,7 @@ view_time time =
     seconds = String.fromInt <| Time.toSecond zone time
   in
     -- HH:MM:SS
-    E.el [E.centerX, E.centerY] <| E.text (pad hours ++ ":" ++ pad minutes ++ ":" ++ pad seconds)
+    E.el [E.centerX, E.centerY, E.paddingEach { edges | right = 12 }] <| E.text (pad hours ++ ":" ++ pad minutes ++ ":" ++ pad seconds)
 
 -- Makes numbers two-digit; I don't think there's a nice way to pad the left with zeroes
 pad : String -> String
@@ -101,10 +122,10 @@ zone = Time.customZone (1 * 60) []
 
 something : List Entry -> E.Element msg
 something m =
-  m |> List.take 3 |> List.indexedMap entry |> E.column [E.height E.fill, E.centerX, E.centerY]
+  m |> List.take 3 |> List.indexedMap entry |> E.column [E.height E.fill, E.centerX]
 
 entry : Int -> Entry -> E.Element msg
-entry num (time, dest) = E.row [E.width (E.px 350), E.centerY, E.spacing 20] [E.text (String.fromInt num), E.el [E.alignLeft] <| E.text dest, E.el [E.alignRight] <| E.text (String.fromInt (time // 60) ++ " mins")]
+entry num (time, dest) = E.row [E.width (E.px 280), E.centerY, E.spacing 20] [E.text (String.fromInt num), E.el [E.alignLeft] <| E.text dest, E.el [E.alignRight] <| E.text (String.fromInt (time // 60) ++ " mins")]
 
 -- Ravenscourt Park as an example; TODO let user switch
 stop_id = "940GZZLUGPK"
